bc205009060325b3e595d6efcc297edd181cdbd8
RayLight <arusurobo@gmail.com>
2025-03-23 04:33:18 +0900

    add MockMaskCaluculator

---
+++ mask_caluculator/mock_mask_calculator.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from typing import Union
+from mask_calculator import MaskCalculator
+
+
+class MockMaskCaluculator(MaskCalculator):
+
+    def __init__(self, is_all_foreground: bool):
+        self.is_all_foreground = is_all_foreground
+
+    def get_mask(
+        self,
+        frame: cv2.typing.MatLike,
+        background_frame: Union[cv2.typing.MatLike, None],
+    ) -> Union[np.ndarray, None]:
+        """前景を1,背景を0としたマスクを計算する"""
+        if self.is_all_foreground:
+            return np.ones(frame.shape)
+        else:
+            return np.zeros(frame.shape)

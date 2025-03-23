c0f3402c7c6aab65d5e61a952d75ee141c8baf1b
RayLight <arusurobo@gmail.com>
2025-03-24 03:15:28 +0900

    add example

--- camera_parameter.py
+++ camera_parameter.py
@@ -12,6 +12,22 @@ class CameraParameter:
     index: int
 
 
+# camera_parameters:
+#   - fx: 1000.0
+#     fy: 1000.0
+#     cx: 640.0
+#     cy: 480.0
+#     id: "camera_1"
+#     index: 0
+
+#   - fx: 950.0
+#     fy: 950.0
+#     cx: 620.0
+#     cy: 460.0
+#     id: "camera_2"
+#     index: 1
+
+
 def load_camera_params(path: str) -> list[CameraParameter]:
     with open(path) as f:
         data = yaml.safe_load(f)
